276180c1c2bfd8b788c6a099f7035dddf7d59866
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2024-11-30 05:58:54 +0900

    Gpu.Vulkan.Sparse.Middle: memoryBindToCore

--- src/Gpu/Vulkan/Sparse/Middle.hs
+++ src/Gpu/Vulkan/Sparse/Middle.hs
@@ -1,3 +1,7 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-module Gpu.Vulkan.Sparse.Middle where
+module Gpu.Vulkan.Sparse.Middle (
+	MemoryBind(..)
+	) where
+
+import Gpu.Vulkan.Sparse.Middle.Internal
--- src/Gpu/Vulkan/Sparse/Middle/Internal.hs
+++ src/Gpu/Vulkan/Sparse/Middle/Internal.hs
@@ -3,9 +3,11 @@
 
 module Gpu.Vulkan.Sparse.Middle.Internal where
 
-import Gpu.Vulkan.Device.Middle qualified as Device
-import Gpu.Vulkan.Memory.Middle qualified as Memory
+import Gpu.Vulkan.Device.Middle.Internal qualified as Device
+import Gpu.Vulkan.Memory.Middle.Internal qualified as Memory
 import Gpu.Vulkan.Sparse.Enum
+
+import Gpu.Vulkan.Sparse.Core qualified as C
 
 data MemoryBind = MemoryBind {
 	memoryBindResourceOffset :: Device.Size,
@@ -13,3 +15,18 @@ data MemoryBind = MemoryBind {
 	memoryBindMemory :: Memory.M,
 	memoryBindMemoryOffset :: Device.Size,
 	memoryBindFlags :: MemoryBindFlags }
+
+memoryBindToCore :: MemoryBind -> IO C.MemoryBind
+memoryBindToCore MemoryBind {
+	memoryBindResourceOffset = Device.Size ro,
+	memoryBindSize = Device.Size sz,
+	memoryBindMemory = mm,
+	memoryBindMemoryOffset = Device.Size mo,
+	memoryBindFlags = MemoryBindFlagBits fs } = do
+	cmm <- Memory.mToCore mm
+	pure C.MemoryBind {
+		C.memoryBindResourceOffset = ro,
+		C.memoryBindSize = sz,
+		C.memoryBindMemory = cmm,
+		C.memoryBindMemoryOffset = mo,
+		C.memoryBindFlags = fs }
