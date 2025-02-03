a7dbf3a1637ef59bc0bbdcfcfe6f18744ef6b2a0
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-02-03 13:52:36 +0900

    Gpu.Vulkan.Device.GroupDevice.Middle.Internal: createInfoToCore: repair

--- src/Gpu/Vulkan/Device/GroupDevice/Middle/Internal.hs
+++ src/Gpu/Vulkan/Device/GroupDevice/Middle/Internal.hs
@@ -30,20 +30,19 @@ instance WithPoked (TMaybe.M mn) => WithPoked (CreateInfo mn) where
 		f . ptrS $ castPtr pci
 
 createInfoToCore :: WithPoked (TMaybe.M mn) =>
-	CreateInfo mn -> (Ptr C.CreateInfo -> IO a) -> IO ()
+	CreateInfo mn -> (C.CreateInfo -> IO a) -> IO ()
 createInfoToCore CreateInfo {
 	createInfoNext = mnxt,
 	createInfoPhysicalDevices = (id &&& length) -> (pds, pdc)
 	} f =
 	putStrLn "createInfoToCore begin" >>
 	withPoked' mnxt \pnxt -> withPtrS pnxt \(castPtr -> pnxt') ->
-	alloca \pci -> allocaArray pdc \ppds -> do
+	allocaArray pdc \ppds -> do
 		pokeArray ppds (phdToCore <$> pds)
-		poke pci C.CreateInfo {
+		() <$ f C.CreateInfo {
 			C.createInfoSType = (),
 			C.createInfoPNext = pnxt',
 			C.createInfoPhysicalDeviceCount = fromIntegral pdc,
 			C.createInfoPPhysicalDevices = ppds }
-		() <$ f pci
 	where
 	phdToCore (PhysicalDevice.P p) = p
