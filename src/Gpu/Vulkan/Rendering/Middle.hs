e709b7951a7ecf4bd3729250b26b072ece51e110
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2025-02-28 16:04:53 +0900

    Gpu.Vulkan.Rendering.Middle: export class AttachmentInfoToCore

--- src/Gpu/Vulkan/Rendering/Middle.hs
+++ src/Gpu/Vulkan/Rendering/Middle.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Gpu.Vulkan.Rendering.Middle (
-	Info(..), AttachmentInfo(..), attachmentInfoZero ) where
+	Info(..),
+	AttachmentInfo(..), attachmentInfoZero, AttachmentInfoToCore ) where
 
 import Gpu.Vulkan.Rendering.Middle.Internal
